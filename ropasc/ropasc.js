dbaff071b4697b8775730a5f006aca37abdddd3e
Sidney <sidneypang@hotmail.co.uk>
2022-02-25 19:16:50 -0500

    finished with button and 5 round message

--- ropasc/ropasc.js
+++ ropasc/ropasc.js
@@ -1,7 +1,8 @@
 let choice = ["rock", "paper", "scissor"];
-
 let playerWin = 0;
 let computerWin = 0;
+
+let count = 0;
 
 
 
@@ -11,44 +12,41 @@ function computerPlay (choice) {
 }
 
 
-
-
-
 function playRound (playerSelection, computerSelection) {
 
     if (playerSelection.toUpperCase() === "ROCK") {
         if (computerSelection.toUpperCase() === "ROCK") {
-            return "Tie!";
+            return "Draw! The score is " + playerWin + " to " + computerWin + ".";
         } else if (computerSelection.toUpperCase() === "PAPER") {
             computerWin ++;
-            return "You Lose! Paper beats Rock";
+            return "You Lose! Paper beats Rock. The score is " + playerWin + " to " + computerWin + ".";
         } else {
             playerWin ++;
-            return "You Win! Rock beats Scissor";
+            return "You Win! Rock beats Scissor. The score is " + playerWin + " to " + computerWin + ".";
         }
 
 
     } else if (playerSelection.toUpperCase() === "PAPER") {
         if (computerSelection.toUpperCase() === "PAPER") {
-            return "Tie!!";
+            return "Draw! The score is " + playerWin + " to " + computerWin + ".";
         } else if (computerSelection.toUpperCase() === "ROCK") {
             playerWin ++;
-            return "You Win! Paper beats Rock";
+            return "You Win! Paper beats Rock. The score is " + playerWin + " to " + computerWin + ".";
         } else {
             computerWin ++;
-            return "You Lose! Scissor beats Paper";
+            return "You Lose! Scissor beats Paper. The score is " + playerWin + " to " + computerWin + ".";
         }
 
 
     } else if (playerSelection.toUpperCase() === "SCISSOR") {
         if (computerSelection.toUpperCase() === "SCISSOR") {
-            return "Tie";
+            return "Draw! The score is " + playerWin + " to " + computerWin + ".";
         } else if (computerSelection.toUpperCase() === "PAPER") {
             playerWin ++;
-            return "You Win! Scissor beats Paper";
+            return "You Win! Scissor beats Paper. The score is " + playerWin + " to " + computerWin + ".";
         } else {
             computerWin ++;
-            return "You Lose! Rock beats Scissor";
+            return "You Lose! Rock beats Scissor. The score is " + playerWin + " to " + computerWin + ".";
         }
     } else {
         return "Not an option"
@@ -56,71 +54,74 @@ function playRound (playerSelection, computerSelection) {
 }
 
 
+//this is for making 5 rounds in console
 
-// function playRound (playerSelection, computerSelection) {
-
-//     if (playerSelection.toUpperCase() === "ROCK") {
-//         if (computerSelection.toUpperCase() === "ROCK") {
-//             console.log ("Tie!");
-//         } else if (computerSelection.toUpperCase() === "PAPER") {
-//             console.log ("You Lose! Paper beats Rock");
-//         } else {
-//             console.log ("You Win! Rock beats Scissor");
-//         }
+// function game () {
 
 
-//     } else if (playerSelection.toUpperCase() === "PAPER") {
-//         if (computerSelection.toUpperCase() === "PAPER") {
-//             console.log ("Tie!!");
-//         } else if (computerSelection.toUpperCase() === "ROCK") {
-//             console.log ("You Win! Paper beats Rock");
-//         } else {
-//             console.log ("You Lose! Scissor beats Paper");
-//         }
+//     for (let i = 0; i < 5; i++) {
 
+//         let playerSelection = prompt ("Rock, paper or scissor?");
+//         let computerSelection = computerPlay(choice);
 
-//     } else if (playerSelection.toUpperCase() === "SCISSOR") {
-//         if (computerSelection.toUpperCase() === "SCISSOR") {
-//             console.log ("Tie");
-//         } else if (computerSelection.toUpperCase() === "PAPER") {
-//             console.log ("You Win! Scissor beats Paper");
-//         } else {
-//             console.log ("You Lose! Rock beats Scissor");
-//         }
+//         console.log (playRound (playerSelection, computerSelection));
+
+//         console.log ("The score is " + playerWin + " to " + computerWin + ".");
+        
+//     }
+
+//     if (playerWin > computerWin) {
+//         return "You won the game! It was " + playerWin + " to " + computerWin;
+//     } else if (playerWin < computerWin) {
+//         return "You lost the game! It was " + playerWin + " to " + computerWin;
 //     } else {
-//         console.log ("Not an option")
-//     }    
+//         return "It's a tie! It was " + playerWin + " to " + computerWin;
+//     }
+
 // }
 
 
 
 
+const buttons = document.querySelectorAll ("button");
 
-function game () {
+buttons.forEach ((button) => {
+    button.addEventListener ("click", () => {
 
-
-    for (let i = 0; i < 5; i++) {
-
-        let playerSelection = prompt ("Rock, paper or scissor?");
+        
+        let playerSelection = button.id;
         let computerSelection = computerPlay(choice);
 
-        console.log (playRound (playerSelection, computerSelection));
-
-        console.log ("The score is " + playerWin + " to " + computerWin + ".");
         
-    }
 
-    if (playerWin > computerWin) {
-        return "You won the game! It was " + playerWin + " to " + computerWin;
-    } else if (playerWin < computerWin) {
-        return "You lost the game! It was " + playerWin + " to " + computerWin;
-    } else {
-        return "It's a tie! It was " + playerWin + " to " + computerWin;
-    }
+        let newDiv = document.createElement ("div");
+        newDiv.classList.add ("content");
+        document.body.appendChild (newDiv);
+        newDiv.textContent = playRound (playerSelection, computerSelection);
 
-}
+        count++
+        console.log (count);
+        console.log (playerWin);
+        
 
-
-
-
-console.log (game())
+        if (count === 5) {
+            if (playerWin > computerWin) {
+                playerWin = 0;
+                computerWin = 0;
+                count = 0;
+                alert ( "You won the game! It was " + playerWin + " to " + computerWin);
+            } else if (playerWin < computerWin) {
+                playerWin = 0;
+                computerWin = 0;
+                count = 0;
+                alert ( "You lost the game! It was " + playerWin + " to " + computerWin);
+            } else {
+                playerWin = 0;
+                computerWin = 0;
+                count = 0;
+                alert ( "It's a tie! It was " + playerWin + " to " + computerWin);
+            }
+        }
+        
+    });
+});
